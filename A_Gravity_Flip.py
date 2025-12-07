8d17cc8474b8ad718626547569dce59e5751f6bc
SubashSK777 <subashsk11831@gmail.com>
2025-12-07 20:21:45 +0530

    Auto commit at 12/7/2025, 8:21:45 PM

--- A_Gravity_Flip.py
+++ A_Gravity_Flip.py
@@ -5,8 +5,9 @@ arr = list(map(int, input().split()))
 for j in range(n):
     for i in range(n - 1):
         if arr[i] > arr[i + 1]:
-            arr[i + 1] += arr[i] - arr[i + 1]
-            arr[i] -= arr[i] - arr[i + 1]
+            dif = arr[i] - arr[i + 1]
+            arr[i + 1] += dif
+            arr[i] -= dif
         else:
             continue
 
