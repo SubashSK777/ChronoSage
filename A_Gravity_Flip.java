a180b11f3aee9a726af6aa7746d035a866fecb4a
SubashSK777 <subashsk11831@gmail.com>
2025-12-07 20:31:45 +0530

    Auto commit at 12/7/2025, 8:31:45 PM

--- A_Gravity_Flip.java
+++ A_Gravity_Flip.java
@@ -4,6 +4,26 @@ class A_Gravity_Flip{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < n - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    int dif = arr[j] - arr[j + 1];
+
+                    arr[j + 1] += dif;
+
+                    arr[j] -= dif;
+                }
+            }
+        }
+
+        for(int i : arr) System.out.print(i + " ");
     }
 }
